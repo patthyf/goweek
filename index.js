3ad06a082679683e8f3bc38fbead555d7505d26a
patthyf <patthyfeliciano@gmail.com>
2018-11-07 00:05:27 -0200

    .
    .

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require ("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+
+mongoose.connect(
+    "mongodb://<dbuser>:<dbpassword>@ds155073.mlab.com:55073/goweek-patricia",
+    { 
+        useNewUrlParser:true    
+    }
+);
+
+app.use((req,res, next) => {
+    req.io = io;
+
+    return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes"));
+
+server.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
